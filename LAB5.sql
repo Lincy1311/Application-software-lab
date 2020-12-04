3b524241c2be5c755f776dcc4248295646dd596c
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2020-12-04 14:43:13 +0530

    Add files via upload

---
+++ LAB5.sql
@@ -0,0 +1,21 @@
+CREATE TABLE class(
+                  id VARCHAR(3),
+                  name CHAR(20)
+);
+INSERT INTO class VALUES
+('a1','Lincy'),
+('a2','James');
+SELECT * FROM class;
+SET autocommit=0;
+START TRANSACTION;
+INSERT INTO class VALUES
+('a3','Levi');
+SELECT * FROM class;
+SAVEPOINT class1;
+INSERT INTO class VALUES
+('a4','Aswin');
+SELECT * FROM class;
+SAVEPOINT class2;
+ROLLBACK TO class1;
+SELECT * FROM class;
+COMMIT;
